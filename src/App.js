58163cca279e76eff8d18da138a777a9f7c4ff90
nemviktor <wookie7373@gmail.com>
2021-09-07 01:02:14 +0200

    First attempt to fetch from eaternity. Not successfull.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import NavBar from './components/ui/NavBar';
 import Main from './components/ui/Main';
 import CarbonEstimates from './components/CarbonEstimates';
+import Recipes from './components/Recipes';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Main/>
-      <CarbonEstimates/>
+      {/* <CarbonEstimates/> */}
+      <Recipes />
     </div>
   );
 }
