211ce6bbbc98fefa3259d460e9d804d51564f429
nemviktor <wookie7373@gmail.com>
2021-09-07 23:01:45 +0200

    Fetching data from clover API. Trying to grab data through form.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import NavBar from './components/ui/NavBar';
 import Main from './components/ui/Main';
 import CarbonEstimates from './components/CarbonEstimates';
-import Recipes from './components/Recipes';
+import GroundTransport from './components/GroundTransport';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Main/>
+      {/* <Main/> */}
       {/* <CarbonEstimates/> */}
-      <Recipes />
+      <GroundTransport />
     </div>
   );
 }
---
+++ src/components/GroundTransport.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const URL = 'https://api.cloverly.com/2019-03-beta/estimates/vehicle';
+
+const options = {
+    method: 'post',
+    body: '',
+    headers: {
+        'Content-type' : 'application/json',
+        "Authorization" : "Bearer public_key:47800ea0ee541b4c"
+    }
+};
+
+const GroundTransport = props => {
+
+    const [distanceTravelled, setDistanceTravelled] = useState(100);
+    const [co2InKg , setCo2InKg] = useState(0);
+
+    useEffect(() => {
+        let body = JSON.stringify({"distance":{"value": { distanceTravelled },"units":"km"},"fuel_efficiency":{"value":23.5215,"units":"mpg","of":"gasoline"}});
+        options.body = body;
+        fetch(URL, options )
+        .then(response => response.json())
+        .then(resp => {
+            setCo2InKg(resp.equivalent_carbon_in_kg);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }, [ distanceTravelled ])
+
+
+    const handleGivenDistance = event => {
+        let distance = event.target.value;
+        setDistanceTravelled(distance); 
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("yippijájé");
+      };
+
+    return (
+        <div className="ground-transport-form">
+            { distanceTravelled }
+            <fieldset>
+                <legend >Ground transportation</legend>
+                <form id="ground-transport-form">
+                    <input placeholder="Give me the distance in km-s" value={ this.co2InKg } onChange={ handleGivenDistance }></input><br />
+                    <input placeholder="Average consumption-not active yet(assumed 10 l/100km)"></input><br />
+                    <select name="fuel-types" id="fuel-types">
+                        <option value="gasoline">gasoline</option>
+                        <option value="diesel">diesel</option>
+                    </select><br />
+                    <button type="button" onClick={ handleSubmit }>Calculate</button>
+                </form>
+
+                <p>Your carbon consumption with this travel:</p>
+                <p> { co2InKg + ' kg' } </p>
+            </fieldset>
+        </div>
+    )
+}
+
+export default GroundTransport;
