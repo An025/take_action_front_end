1441196ab35ce981825945e17db70f1d751aacf0
nemviktor <wookie7373@gmail.com>
2021-09-08 01:03:44 +0200

    Creating first version of flight calculation function. Fetching data.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import NavBar from './components/ui/NavBar';
 import Main from './components/ui/Main';
 import CarbonEstimates from './components/CarbonEstimates';
+import FlightTransport from './components/FlightTransport';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Main/>
-      <CarbonEstimates/>
+      {/* <Main/> */}
+      {/* <CarbonEstimates/> */}
+      <FlightTransport />
     </div>
   );
 }
---
+++ src/components/FlightTransport.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+
+const URL = 'https://api.cloverly.com/2019-03-beta/estimates/flight';
+
+const listOfBiggestAirports = [
+    '','CAN','ATL','CTU','DFW','SZX','CKG','PEK','DEN','KMG','SHA','XIY','HND','ORD','PVG','LAX',
+    'DEL','HGH','CLT','DXB','IST','CDG','LHR','MEX','PHX','SGN','MCO','CGO','CJU','AMS','GRU',
+    'SEA','NKG','SVO','CSX','FRA','MIA','IAH','GMP','MAD','SAW','XMN','BKK','JFK','KWE','HAK',
+    'FLL','SFO','DME','PKX','EWR',
+];
+
+const options = {
+    method: 'post',
+    body: '',
+    headers: {
+        'Content-type' : 'application/json',
+        "Authorization" : "Bearer public_key:47800ea0ee541b4c"
+    }
+};
+
+const FlightTransport = props => {
+
+    const [distanceFlew, setDistanceFlew] = useState(0);
+    const [co2InKg , setCo2InKg] = useState(0);
+    const [airports,setAirports] = useState([])
+
+    useEffect(() => {
+        let body = JSON.stringify({"airports":["sfo","atl","fra"]});
+        options.body = body;
+
+        fetch(URL, options )
+        .then(response => response.json())
+        .then(resp => {
+            setDistanceFlew(resp.distance_in_miles * 1.6)
+            setCo2InKg(resp.equivalent_carbon_in_kg)
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }, [ ])
+
+
+    const handleSubmit = (event) => {
+        /* let distance = event.target.parentNode.firstChild.value;
+        setAirports(airports);  */
+      };
+
+    return (
+        <div className="flight-transport-form">
+            <fieldset>
+                <legend >Flight transportation</legend>
+                <form id="flight-transport-form">
+                    <label for="airportsFrom">Airport from:</label>
+                    <select name="airportsFrom" id="airportsFrom">
+                        { listOfBiggestAirports.map((airport) => 
+                            <option value={ airport }> { airport } </option>
+                        )}
+                    </select>
+
+                    <label for="airportsThrough">Airport through:</label>
+                    <select name="airportsThrough" id="airportsThrough">
+                        { listOfBiggestAirports.map((airport) => 
+                            <option value={ airport }> { airport } </option>
+                        )}
+                    </select>
+
+                    <label for="airportsTo">Airport to:</label>
+                    <select name="airportsTo" id="airportsTo">
+                        { listOfBiggestAirports.map((airport) => 
+                            <option value={ airport }> { airport } </option>
+                        )}
+                    </select><br />
+                    <button type="button" onClick={ handleSubmit }>Calculate</button>
+                </form>
+                <p>You flew { distanceFlew } km-s.</p>
+                <p>Your carbon consumption with this travel was { co2InKg } kg-s.</p>
+            </fieldset>
+        </div>
+    )
+}
+
+export default FlightTransport;
+
