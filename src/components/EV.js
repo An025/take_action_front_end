cd7446ddb29f0b4e92991888580dc367756d46e6
Ani <jancso.anita@gmail.com>
2021-09-11 08:57:37 +0200

    Fetch station and display on the map

--- src/components/EV.js
+++ src/components/EV.js
@@ -14,6 +14,25 @@ export default function EV(){
     scrollZoom:true,
   });
 
+  const [EV, setEV] = useState([]);
+  const [selectedStation, setSelectedStation] = useState(null);
+  useEffect(()=>{
+
+  
+  fetch('https://api.openchargemap.io/v3/poi/', {
+      mode: 'cors', 
+      headers: {
+        'x-api-key': '1ef9e57e-d6a4-4b3a-80b5-1719b8c7b5db',
+        'User-Agent' : 'My-App',
+        'Accept': '*/*',
+      },
+    })
+    .then(response => response.json())
+    .then(data => setEV(data))
+    .catch(error => console.log('Error while fetching:', error))
+  
+  }, [])
+ 
 
   
   return (<div>
@@ -23,6 +42,22 @@ export default function EV(){
       onViewportChange={viewport =>{
         setViewport(viewport);
       }}>
+           {EV.map((station)=>(
+          <Marker
+            key={station.ID + station.AddressInfo.Title}
+            latitude={station.AddressInfo.Latitude}
+            longitude={station.AddressInfo.Longitude}
+          >
+            <button className="marker-btn" onClick={(e) =>{
+            e.preventDefault();
+            setSelectedStation(station);
+          }}>
+            
+            <img src="/charger.svg" alt="charger"/>
+            </button>
+          </Marker>
+        ))}
+     
     </ReactMapGl>
   </div>
   )
