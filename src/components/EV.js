ffc93a2a4e12b4419e8dc4ee79f8b93a1b068845
Ani <jancso.anita@gmail.com>
2021-09-11 08:56:07 +0200

    Display mapbox on the screen

--- src/components/EV.js
+++ src/components/EV.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
+import ReactMapGl, {Marker, Popup} from 'react-map-gl'
+import './EV.css';
 
 export default function EV(){
-    return <div>Hello</div>
+  const [viewport, setViewport] = useState({
+    latitude: 51.545581,
+    longitude: -0.077301,
+    // latitude: 47.497913,
+    // longitude: 19.040236,
+    width: '100vw',
+    height: '89vh',
+    zoom: 10,
+    scrollZoom:true,
+  });
+
+
+  
+  return (<div>
+    <ReactMapGl {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/enviroso/cktd9xlyz00t417pdy461b3mx"
+      onViewportChange={viewport =>{
+        setViewport(viewport);
+      }}>
+    </ReactMapGl>
+  </div>
+  )
 }
+
