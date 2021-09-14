48807536d392a761237d663cb4e445725140716b
nemviktor <wookie7373@gmail.com>
2021-09-14 23:04:12 +0200

    Adding dim effect to navbar.

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -2,40 +2,54 @@ import './NavBar.scss';
 // import logo from "../images/logo.gif"
 import logo from "../images/leaf_logo.png"
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 // import Button from '@material-ui/core/Button';
-import DropDown from './elements/DropDown'
 import {Link} from "react-router-dom";
+import styled from 'styled-components';
+
+
+const DimContainer = styled.div`
+  display: none;
+  position:fixed;
+  left:0;
+  top:10vh;
+  z-index: 5;
+  height: 100%;
+  width: 100%;
+  background: rgb(0,0,0,0.6);
+  &:hover {function(e) {
+    display: block;
+  }}
+`;
 
 
 export default function NavBar() {
-  
 
   return (
-    <div className="navbar" >
-      
-      <div className="leftContainer">
-        <img className="logo" src={ logo } alt="logo"/>
-        <Link to="/">
-          <h4 className="title">Enviroso</h4>
-        </Link>
-        <Link to="/side">
-          <h4 className="title"> Footprint</h4>
-        </Link>
-        <Link to="/side">
-          <h4 className="title">About Us</h4>
-        </Link>
-      </div>
-      <div className="rightContainer">
-        <Link to="/login">
-          <h4 className="title">Login</h4>
-        </Link>
+    <div className="main-container">
+
+      <div className="navbar">
+        <div className="leftContainer">
+          <img className="logo" src={ logo } alt="logo"/>
+          <Link to="/">
+            <h4 className="title">Enviroso</h4>
+          </Link>
+          <Link to="/side">
+            <h4 className="title"> Footprint</h4>
+          </Link>
+          <Link to="/side">
+            <h4 className="title">About Us</h4>
+          </Link>
+        </div>
+        <div className="rightContainer">
+          <Link to="/login">
+            <h4 className="title">Login</h4>
+          </Link>
+        </div>
       </div>
 
-      {/* <Typography variant="h6" className="title">Enviroso</Typography>
-      <div className="menuItems">
+      <div className="dim-container">
+      </div>
 
-      </div> */}
     </div>
   );
 }
