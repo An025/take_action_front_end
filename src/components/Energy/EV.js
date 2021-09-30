a6f6a114c28d175d233395c83ab697f0e76d4cb8
Ani <jancso.anita@gmail.com>
2021-09-30 13:10:27 +0200

    Rewrite api call and add heart to popup window

--- src/components/Energy/EV.js
+++ src/components/Energy/EV.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from "react";
 import ReactMapGl, {Marker, Popup} from 'react-map-gl'
 import './EV.css';
 import axios from 'axios';
+import FavoriteBorderIcon from '@material-ui/icons//FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 export default function EV(){
   const [viewport, setViewport] = useState({
@@ -11,11 +13,12 @@ export default function EV(){
     longitude: 19.040236,
     width: '100vw',
     height: '89vh',
-    zoom: 10,
+    zoom: 13,
     scrollZoom:true,
   });
 
- 
+  const [favorite, setFavorite] = useState()
+  const toggleHeart = () => setFavorite(!favorite)
   const [EV, setEV] = useState([]);
   const [selectedStation, setSelectedStation] = useState(null);
   const axiosHeader = {
@@ -58,9 +61,10 @@ export default function EV(){
       }}>
            {EV.map((station)=>(
           <Marker
-            key={station.ID + station.AddressInfo.Title}
-            latitude={station.AddressInfo.Latitude}
-            longitude={station.AddressInfo.Longitude}
+            key={station.evId + station.latitude}
+            latitude={station.latitude}
+            longitude={station.longitude}
+            favorite={station.favorite}
           >
             <button className="markerBtn" onClick={(e) =>{
             e.preventDefault();
@@ -73,16 +77,21 @@ export default function EV(){
         ))}
            {selectedStation ?(
         <Popup
-        latitude={selectedStation.AddressInfo.Latitude}
-        longitude={selectedStation.AddressInfo.Longitude}
+        latitude={selectedStation.latitude}
+        longitude={selectedStation.longitude}
+        
         onClose={()=>{
           setSelectedStation(null);
         }}>
         
           <div className="popup">
-            <h2>{selectedStation.AddressInfo.Title}</h2>
-            <p>{selectedStation.AddressInfo.Town}</p>
-            <p>{selectedStation.AddressInfo.Connections}</p>
+            <h2>{selectedStation.title}</h2>
+            <p>{selectedStation.town}</p>
+            <p>{selectedStation.address}</p>
+            <p>{selectedStation.favorite}</p>
+            <p onClick={toggleHeart} className="favorite"> 
+            {favorite?  <FavoriteIcon/>: <FavoriteBorderIcon/>}
+               </p>
           </div>
         </Popup>
       ) : null }
