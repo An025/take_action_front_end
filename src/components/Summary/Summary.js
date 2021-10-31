6c810b8aed267cc9725da58e73ba9983ca9e44e1
peterolah-tech <peter.olah.hu@gmail.com>
2021-10-31 13:08:55 +0100

    Add recipe stats to common summary

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import EVSummary from './EVSummary';
+import RecipeSummary from './RecipeSummary';
 
 const Summary = props =>{
 
 
 return(
     <div>
-
         <EVSummary/>
+        <RecipeSummary/>
     </div>
     )
 }
