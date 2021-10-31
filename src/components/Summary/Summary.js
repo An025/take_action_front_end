6306486c8f1a82648b9c132384c1c83641434385
Ani <jancso.anita@gmail.com>
2021-10-31 09:50:50 +0100

    Add animated footprint to summary

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import EVSummary from './EVSummary';
+import footprint from '../images/footprint.gif'
+
+import  './Footprint.scss';
+
 
 const Summary = props =>{
 
@@ -8,6 +12,8 @@ return(
     <div>
 
         <EVSummary/>
+        <img className="footprint" src={ footprint } alt="footprint" ></img>
+
     </div>
     )
 }
